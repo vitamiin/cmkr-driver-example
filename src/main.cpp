ae20e724ec3c315685e3d54e42f5c5be5bd34e36
vitamiin <mailthis2tom@gmail.com>
2023-01-14 15:28:22 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <ntddk.h>
+
+void unload_routine(PDRIVER_OBJECT driver_object) {
+    UNREFERENCED_PARAMETER(driver_object);
+
+    DbgPrint("- ZwSchool driver unloaded -\n");
+}
+
+extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+    DriverObject->DriverUnload = unload_routine;
+
+    return STATUS_SUCCESS;
+}
