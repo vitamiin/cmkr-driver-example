dfa1fb63a0137955600cf9dfb97f4624f37bd9a5
vitamiin <mailthis2tom@gmail.com>
2023-01-14 15:37:17 +0200

    Changed some copy-pase code from another repo

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@
 void unload_routine(PDRIVER_OBJECT driver_object) {
     UNREFERENCED_PARAMETER(driver_object);
 
-    DbgPrint("- ZwSchool driver unloaded -\n");
+    DbgPrint("- Cmkr driver unloaded -\n");
 }
 
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
     UNREFERENCED_PARAMETER(RegistryPath);
 
     DriverObject->DriverUnload = unload_routine;
+
+    DbgPrint("Hello from a cmkr kernel driver!");
 
     return STATUS_SUCCESS;
 }
